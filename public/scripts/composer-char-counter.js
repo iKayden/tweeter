7ad74b41a7c88917433da27401b3684ea93b3925
ikayden <ikyden23@gmail.com>
2022-10-09 14:02:07 -0700

    Made and filled with test content a TWEET CONTAINER. Made CSS styles for it and it looks great. So far No MAJOR bugs or problems. Good commit.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
-  $("#tweet-text").on("input", function(e) {
-    // we use available "this" to target text area
+  $("#tweet-text").on("input", function (e) {
+
+    // we use available context of "this" to be the targeted text area
     const charLength = $(this).val().length;
     let charLimit = 140 - charLength;
+
     // we traverse the DOM from "this"/textarea node to the "counter" node
     const charCounter = $(this).siblings().children(".counter");
+
     // To change html representation of the numbers left on the page
     charCounter.html(charLimit);
-    // if user goes above the limit we will add class to make negative numbers Red
+
+    // if user goes above the limit we will add class to make negative numbers color:Red
+    // using the css class "counter-negative" (in new-tweet.css)
     charCounter.toggleClass("counter-negative", charLimit < 0);
   });
 });
