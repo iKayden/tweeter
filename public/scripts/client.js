b255f109bccd5d00d346cad07a6ce99face52355
ikayden <ikyden23@gmail.com>
2022-10-12 16:32:54 -0700

    Added loadTweets function to upload tweets from the Data Base. Added timeago.js library to parse the tome data into readable and nice format.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,71 +1,65 @@
 $(document).ready(function () {
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac",
-      },
-      "content": {
-        "text":
-          "If I have seen further it is by standing on the shoulders of giants",
-      },
-      "created_at": 1461116232227,
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd",
-      },
-      "content": {
-        "text": "Je pense , donc je suis",
-      },
-      "created_at": 1461113959088,
-    },
-  ];
-
+  // Fetching tweets function
   const createTweetElement = (tweet) => {
     // Write HTML markup to render our data from DB
     const $tweetArticle = $(`<article class="tweet-list-item">
       <header class="tweet-list-header">
-        <span class="tweet-list-header-wrap">
-          <span class="tweet-list-avatar-name"> 
-            <img class="tweet-list-avatar" src="${tweet.user.avatars}" alt="Profile picture"/>
-            <p class="tweet-list-username">${tweet.user.name}</p>
-          </span>
-          <p class="tweet-list-handle">${tweet.user.handle}</p>
-        </span>
-        <span class="tweet-list-text">${tweet.content.text}    
-        </span>
+      <span class="tweet-list-header-wrap">
+      <span class="tweet-list-avatar-name"> 
+      <img class="tweet-list-avatar" src="${tweet.user.avatars}" alt="Profile picture"/>
+      <p class="tweet-list-username">${tweet.user.name}</p>
+      </span>
+      <p class="tweet-list-handle">${tweet.user.handle}</p>
+      </span>
+      <span class="tweet-list-text">${tweet.content.text}    
+      </span>
       </header>
       <footer>
-        <span>${tweet.created_at} days ago</span>
-        <span class="tweet-list-footer-icons">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart-circle-plus"></i>
+      <span>${timeago.format(tweet.created_at)}</span>
+      <span class="tweet-list-footer-icons">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart-circle-plus"></i>
         </span>
-      </footer>`);
+        </footer>`);
 
     return $tweetArticle;
   };
-  // Save the container for rendering our tweets
-  const $tweetsContainer = $("#tweets-container")
 
+  // Save the container for rendering our tweets
+  const $tweetsContainer = $("#tweets-container");
+  // Function for rendering an array of objects (tweets) on the page
   const renderTweets = function (tweets) {
     // loop through tweets
-    for(const tweet of tweets) {
-      // calls createTweetElement for each tweet
+    for (const tweet of tweets) {
+      // calls HTML markup building function for each tweet
       const $tweet = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
+      // takes a return value and adds it to the top of tweets container
       $($tweetsContainer).prepend($tweet);
     }
   };
 
+  // Tweet Form GET route handler by jQuery
+  const $loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+    })
+      .then((tweets) => {
+        // calling the looping and HTML handling function that will do 
+        // all the work needed on each tweet in the Data Base
+        renderTweets(tweets);
+      })
+      .catch((err) => {
+        console.log(`We have an Error ${err}`);
+      });
+  };
+
+  $loadTweets();
+
+  // Form POST route handler by jQuery
   const $tweetForm = $("#tweet-form");
-  $tweetForm.on('submit', (e) => {
+  $tweetForm.on("submit", (e) => {
     // prevent browser from reloading
     e.preventDefault();
     // Parse/encode data into usable format
@@ -74,15 +68,13 @@ $(document).ready(function () {
     $.ajax({
       url: "/tweets",
       method: "POST",
-      data: parsedData
+      data: parsedData,
     })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log("Error:",err);
-    })
-  })
-
-  renderTweets(data);
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(`We have an Error ${err}`);
+      });
+  });
 });
