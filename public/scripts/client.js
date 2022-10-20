7248fc160119607ef2cb7f9bfcaec9ac4a8c8691
ikayden <ikyden23@gmail.com>
2022-10-19 21:11:33 -0700

    Fix two major issues. Previous tweets duplication on a new tweet. error popups now work properly. Looking into fixing CSS on text in submitted tweet where it goes out of borders

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // Cross-site Scripting protection
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
@@ -13,8 +13,7 @@ $(document).ready(function () {
       <header class="tweet-list-header">
       <span class="tweet-list-header-wrap">
       <span class="tweet-list-avatar-name"> 
-      <img class="tweet-list-avatar" src="${
-        tweet.user.avatars
+      <img class="tweet-list-avatar" src="${tweet.user.avatars
       }" alt="Profile picture"/>
       <p class="tweet-list-username">${tweet.user.name}</p>
       </span>
@@ -38,7 +37,8 @@ $(document).ready(function () {
   // Save the container for rendering our tweets
   const $tweetsContainer = $("#tweets-container");
   // Function for rendering an array of objects (tweets) on the page
-  const renderTweets = function (tweets) {
+  const renderTweets = function(tweets) {
+    $tweetsContainer.empty();
     // loop through existing tweets
     for (const tweet of tweets) {
       // calls HTML markup building function for each tweet
@@ -47,78 +47,82 @@ $(document).ready(function () {
       $($tweetsContainer).prepend($tweet);
     }
   };
-  
-    // Stylish Error Pop up setup
-    const $alertDiv = $("#alert-popup");
-    // Error handling functions 
-    // Added two for more responsive way
-    $(function(){ // !(Err Handling Fix) One with fading out on delay
-      $alertDiv.delay(4000).fadeOut();
-    });
-    // Works but the delay function works only once without page reload
-    $alertDiv.on("click", () => { //Second one making it hide on click
-      $alertDiv.hide();
-      
-    }); 
 
+
+  
   // Navbar tweet area toggle
   const $navbarToggleBtn = $("#navbar-toggle");
   $navbarToggleBtn.on("click", () => {
     $tweetForm.toggle();
-    if ($tweetForm.is(":visible")){
-    $("#tweet-text").focus();//user can begin typing right after clicking the button
-  }});
-
+    if ($tweetForm.is(":visible")) {
+      $("#tweet-text").focus(); //user can begin typing right after clicking the button
+    }
+  });
+  
   // Tweet Form GET route handler by jQuery
-  const $loadTweets = function () {
+  const $loadTweets = function() {
     $.ajax({
       url: "/tweets",
       method: "GET",
     })
-      .then((tweets) => {
-        // calling the looping and HTML handling function that will do
-        // all the work needed on each tweet in the Data Base
-        renderTweets(tweets);
-      })
-      .catch((err) => {
-        console.log(`We have an Error ${err}`);
-      });
+    .then((tweets) => {
+      // calling the looping and HTML handling function that will do
+      // all the work needed on each tweet in the Data Base
+      renderTweets(tweets);
+    })
+    .catch((err) => {
+      console.log(`We have an Error ${err}`);
+    });
   };
   $loadTweets();
-
+  
+  const $alertDiv = $("#alert-popup");
+  // Works but the delay function works only once without page reload
+  $alertDiv.on("click", () => {
+    //Second one making it hide on click
+    $alertDiv.hide();
+  });
+ 
   // Form POST route handler by jQuery
   const $tweetForm = $("#tweet-form");
-  $tweetForm.on("submit", (e) => { //Capturing submitting phase event
-    e.preventDefault();  // preventing browser from reloading
+  $tweetForm.on("submit", (e) => {
+    //Capturing submitting phase event
+    e.preventDefault(); // preventing browser from reloading
     
     // Capturing the text area for the tweets to do the validation
     // before the serializing is even made
     const $textValue = $("#tweet-text").val().trim();
-    if (!$textValue) { // ! Problem Decomposition Fix
-      $alertDiv.text("Tweet can't be empty")
+    if (!$textValue) {
+      // ! Problem Decomposition Fix
+      $alertDiv
+      .text("Tweet can't be empty")
       .addClass("error-popup") // adding CSS markup for alert in our popup div
-      .show() // showing us the popup alert if it was used and closed before
+        .show(); // showing us the popup alert if it was used and closed before
+      $alertDiv.delay(4000).fadeOut();
       return;
     } else if ($textValue.length > 140) {
-      $alertDiv.text("Tweet exceeds maximum length of the characters")
-      .addClass("error-popup") // adding CSS markup for alert in our popup div
-      .show() // showing us the popup alert if it was used and closed before
+      $alertDiv
+        .text("Tweet exceeds maximum length of the characters")
+        .addClass("error-popup") // adding CSS markup for alert in our popup div
+        .show(); // showing us the popup alert if it was used and closed before
+      $alertDiv.delay(4000).fadeOut();
       return;
     }
 
     // After validation is done we will parse/encode data into usable format
     const parsedData = $tweetForm.serialize();
-    $.ajax({ // Create our own POST route handler based on jQuery
+    $.ajax({
+      // Create our own POST route handler based on jQuery
       url: "/tweets",
       method: "POST",
-      data: parsedData, // sending our tweet/data object with the response 
+      data: parsedData, // sending our tweet/data object with the response
     })
-    // render/prepend the tweet to the top of the page without reloading
-      .then((tweet) => { 
-        $loadTweets(tweet); 
+      // render/prepend the tweet to the top of the page without reloading
+      .then((tweet) => {
+        $loadTweets(tweet);
         // clean up the text area after the tweet has been posted
         $tweetForm[0].childNodes[3].value = ""; //traversing DOM for extra speed 8)
-        $(".counter").val(140) // !(Func Req. fix)backs the counter to 140
+        $(".counter").val(140); // !(Func Req. fix)backs the counter to
       })
       .catch((err) => {
         console.log(`${err}`);
